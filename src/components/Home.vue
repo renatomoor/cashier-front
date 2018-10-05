af00dc8d9e51d5d0cb209c94a92399a3191b1e98
renatomoor <renato.moor@liip.ch>
2018-10-05 17:54:48 +0200

    First Commit

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>
+        Welcome to the very first commit
+      </h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+
+</style>
